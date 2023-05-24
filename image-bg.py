1bbcc44ccc3a821af0ff3c3aa10f504c217c81a3
Aiyush-G <55917505+Aiyush-G@users.noreply.github.com>
2023-05-24 18:30:03 +0100

    Update image-bg.py backup

--- image-bg.py
+++ image-bg.py
@@ -1,39 +1,113 @@
-import time
+# SPDX-FileCopyrightText: 2019 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
 import board
+import busio
+from digitalio import DigitalInOut
+import adafruit_requests as requests
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from adafruit_esp32spi import adafruit_esp32spi
+
+
+import adafruit_imageload
+import framebufferio
+import rgbmatrix
 import terminalio
-from adafruit_matrixportal.matrixportal import MatrixPortal
+import displayio
 
-# --- Display setup ---
-matrixportal = MatrixPortal(
-    status_neopixel=board.NEOPIXEL,
-    width = 64,
-    height = 64,
-    default_bg = "heatwaves-processed.bmp",
-    debug=True)
+from io import BytesIO
 
-# Create a new label with the color and text selected
-matrixportal.add_text(
-    text_font=terminalio.FONT,
-    text_position=(0, (matrixportal.graphics.display.height // 2) - 1),
-    scrolling=True,
+displayio.release_displays()
+matrix = rgbmatrix.RGBMatrix(
+    width=64, height=64, bit_depth=4,
+    rgb_pins=[
+        board.MTX_R1,
+        board.MTX_G1,
+        board.MTX_B1,
+        board.MTX_R2,
+        board.MTX_G2,
+        board.MTX_B2
+    ],
+    addr_pins=[
+        board.MTX_ADDRA,
+        board.MTX_ADDRB,
+        board.MTX_ADDRC,
+        board.MTX_ADDRD,
+        board.MTX_ADDRE
+    ],
+    clock_pin=board.MTX_CLK,
+    latch_pin=board.MTX_LAT,
+    output_enable_pin=board.MTX_OE
 )
+display = framebufferio.FramebufferDisplay(matrix)
 
-time.sleep(20)
-SCROLL_DELAY = 0.03
 
-contents = [
-    { 'text': 'THIS IS RED',  'color': '#cf2727'},
-    { 'text': 'THIS IS BLUE', 'color': '#0846e4'},
-]
-#matrixportal.set_background("purple.bmp")
-"""
+
+# Get wifi details and more from a secrets.py file
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+print("ESP32 SPI webclient test")
+
+# If you are using a board with pre-defined ESP32 Pins:
+esp32_cs = DigitalInOut(board.ESP_CS)
+esp32_ready = DigitalInOut(board.ESP_BUSY)
+esp32_reset = DigitalInOut(board.ESP_RESET)
+
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+
+requests.set_socket(socket, esp)
+
+if esp.status == adafruit_esp32spi.WL_IDLE_STATUS:
+    print("ESP32 found and in idle mode")
+print("Firmware vers.", esp.firmware_version)
+print("MAC addr:", [hex(i) for i in esp.MAC_address])
+
+for ap in esp.scan_networks():
+    print("\t%s\t\tRSSI: %d" % (str(ap["ssid"], "utf-8"), ap["rssi"]))
+
+print("Connecting to AP...")
+while not esp.is_connected:
+    try:
+        esp.connect_AP(secrets["ssid"], secrets["password"])
+    except OSError as e:
+        print("could not connect to AP, retrying: ", e)
+        continue
+print("Connected to", str(esp.ssid, "utf-8"), "\tRSSI:", esp.rssi)
+print("My IP address is", esp.pretty_ip(esp.ip_address))
+print(
+    "IP lookup adafruit.com: %s" % esp.pretty_ip(esp.get_host_by_name("adafruit.com"))
+)
+print("Ping google.com: %d ms" % esp.ping("google.com"))
+
+#https = requests.Session(socket)
+url = "https://raw.githubusercontent.com/adafruit/Adafruit_CircuitPython_ImageLoad/main/examples/images/4bit.bmp"
+
+print("Fetching text from %s" % url)
+response = requests.get(url)
+print("GET complete")
+bytes_img = BytesIO(response.content)
+
+image, palette = adafruit_imageload.load(bytes_img)
+
+# Create a TileGrid to hold the bitmap
+tile_grid = displayio.TileGrid(image, pixel_shader=palette)
+
+# Create a Group to hold the TileGrid
+group = displayio.Group(scale = 2)
+
+# Add the TileGrid to the Group
+group.append(tile_grid)
+
+# Add the Group to the Display
+display.show(group)
+
+
+
+# Loop forever so you can enjoy your image
 while True:
-    for content in contents:
-        matrixportal.set_text(content['text'])
-
-        # Set the text color
-        matrixportal.set_text_color(content['color'])
-
-        # Scroll it
-        matrixportal.scroll_text(SCROLL_DELAY)
-"""
+    pass

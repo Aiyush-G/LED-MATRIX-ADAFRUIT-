2242f34ffbdd240221e7b5f7b6b9e16e1d0eed48
Aiyush-G <55917505+Aiyush-G@users.noreply.github.com>
2023-05-24 21:20:30 +0100

    Create linkedToAPI.py

---
+++ linkedToAPI.py
@@ -0,0 +1,134 @@
+# SPDX-FileCopyrightText: 2019 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+import board
+import busio
+from digitalio import DigitalInOut
+import adafruit_requests as requests
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from adafruit_esp32spi import adafruit_esp32spi
+
+
+import adafruit_imageload
+import framebufferio
+import rgbmatrix
+import terminalio
+import displayio
+
+from io import BytesIO
+from time import sleep
+
+displayio.release_displays()
+matrix = rgbmatrix.RGBMatrix(
+    width=64, height=64, bit_depth=4,
+    rgb_pins=[
+        board.MTX_R1,
+        board.MTX_G1,
+        board.MTX_B1,
+        board.MTX_R2,
+        board.MTX_G2,
+        board.MTX_B2
+    ],
+    addr_pins=[
+        board.MTX_ADDRA,
+        board.MTX_ADDRB,
+        board.MTX_ADDRC,
+        board.MTX_ADDRD,
+        board.MTX_ADDRE
+    ],
+    clock_pin=board.MTX_CLK,
+    latch_pin=board.MTX_LAT,
+    output_enable_pin=board.MTX_OE
+)
+display = framebufferio.FramebufferDisplay(matrix)
+display.brightness = 0.1
+
+
+# Get wifi details and more from a secrets.py file
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+print("ESP32 SPI webclient test")
+
+# If you are using a board with pre-defined ESP32 Pins:
+esp32_cs = DigitalInOut(board.ESP_CS)
+esp32_ready = DigitalInOut(board.ESP_BUSY)
+esp32_reset = DigitalInOut(board.ESP_RESET)
+
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+
+requests.set_socket(socket, esp)
+
+if esp.status == adafruit_esp32spi.WL_IDLE_STATUS:
+    print("ESP32 found and in idle mode")
+print("Firmware vers.", esp.firmware_version)
+print("MAC addr:", [hex(i) for i in esp.MAC_address])
+
+for ap in esp.scan_networks():
+    print("\t%s\t\tRSSI: %d" % (str(ap["ssid"], "utf-8"), ap["rssi"]))
+
+print("Connecting to AP...")
+while not esp.is_connected:
+    try:
+        esp.connect_AP(secrets["ssid"], secrets["password"])
+    except OSError as e:
+        print("could not connect to AP, retrying: ", e)
+        continue
+print("Connected to", str(esp.ssid, "utf-8"), "\tRSSI:", esp.rssi)
+print("My IP address is", esp.pretty_ip(esp.ip_address))
+print(
+    "IP lookup adafruit.com: %s" % esp.pretty_ip(esp.get_host_by_name("adafruit.com"))
+)
+print("Ping google.com: %d ms" % esp.ping("google.com"))
+
+"""
+bitmap = displayio.OnDiskBitmap("/convert.bmp")
+tile_grid = displayio.TileGrid(bitmap, pixel_shader=bitmap.pixel_shader)
+# Create a Group to hold the TileGrid
+group = displayio.Group(scale = 1)
+
+# Add the TileGrid to the Group
+group.append(tile_grid)
+
+# Add the Group to the Display
+display.show(group)
+"""
+
+
+#https = requests.Session(socket)
+url = "https://elegantscratchyfrontend.aiyushgupta.repl.co/convert"
+
+while True:
+    print("Fetching image from %s" % url)
+    
+    response = requests.get(url)
+    print("GET complete")
+
+    bytes_img = BytesIO(response.content)
+
+    image, palette = adafruit_imageload.load(bytes_img)
+    
+    # Create a TileGrid to hold the bitmap
+    tile_grid = displayio.TileGrid(image, pixel_shader=palette)
+    
+    """
+    except:
+        image = displayio.OnDiskBitmap("/convert.bmp")
+        tile_grid = displayio.TileGrid(image, pixel_shader=image.pixel_shader)
+    """
+
+    # Create a Group to hold the TileGrid
+    group = displayio.Group(scale = 1)
+
+    # Add the TileGrid to the Group
+    group.append(tile_grid)
+
+    # Add the Group to the Display
+    display.show(group)
+    
+    sleep(30)
+
